c2bad98f3b0acb86eb484be4140a76a8d27a78f2
Alex <kccnbnb@mail.ru>
2024-01-15 23:21:26 +0300

    add seminar2

--- src/seminar2/Main.java
+++ src/seminar2/Main.java
@@ -6,8 +6,9 @@ import seminar2.task.Task3;
 
 public class Main {
     public static void main(String[] args) {
-//        Task1.printString(2,'x','y');
-//        Task2.writeInFile();
+       Task1.printString(2,'x','y');
+       Task2.writeInFile();
         Task3.write(".");
+        System.out.println("ddsf");
     }
 }
